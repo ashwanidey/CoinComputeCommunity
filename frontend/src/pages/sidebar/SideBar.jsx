b7c4df5af92f82dca86394d996852ed19d0b7a80
Ashwani <ashwanidey2904@gmail.com>
2024-04-05 02:44:19 +0530

    Reload

--- frontend/src/pages/sidebar/SideBar.jsx
+++ frontend/src/pages/sidebar/SideBar.jsx
@@ -116,7 +116,8 @@ const SideBar = (props) => {
             <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 bottom-0 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" onClick={()=>{
                setIsLoggedIn(false);
                deleteUser()
-               navigate("/feeds")}}>Log Out</button>
+               navigate("/feeds")
+               window.location.reload();}}>Log Out</button>
             </li>
             
             </>}
