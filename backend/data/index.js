49259f2472394ce4ef6564021d2315d36999fbb3
Ashwani <ashwanidey2904@gmail.com>
2024-04-05 13:42:01 +0530

    Added Users

--- backend/data/index.js
+++ backend/data/index.js
@@ -4,6 +4,9 @@ const userIds = [
   new mongoose.Types.ObjectId(),
   new mongoose.Types.ObjectId(),
   new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
+  new mongoose.Types.ObjectId(),
 ];
 
 export const users = [
@@ -36,6 +39,26 @@ export const users = [
     email: "janedoe@gmail.com",
     password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
     picturePath: "3.jpg",
+    
+    __v: 0,
+  },
+  {
+    _id: userIds[3],
+    name: "Jimmy Boss",
+    username: "jimmyBoss",
+    email: "jimmyBoss@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "4.jpg",
+    
+    __v: 0,
+  },
+  {
+    _id: userIds[4],
+    name: "Degen Ape Trader",
+    username: "Degenapetrader",
+    email: "Degenapetrader@gmail.com",
+    password: "$2b$10$dsasdgsagasda//G9JxQ4bQ8KXf4OAIe/X/AK9skyWUy",
+    picturePath: "5.png",
     
     __v: 0,
   },
@@ -119,11 +142,67 @@ export const posts = [
     userId: userIds[2],
     name: "Jane",
     username: "Doe",
-    // location: "Utah, CA",
     description:
       "This is the last really long random description. This one is longer than the previous one.",
     picturePath: "3.jpg",
     isBullish : 'true',
+    // userPicturePath: "p5.jpeg",
+    // likes: new Map([
+    //   [userIds[1], true],
+    //   [userIds[6], true],
+    //   [userIds[3], true],
+    //   [userIds[5], true],
+    // ]),
+    // comments: [
+    //   "one more random comment",
+    //   "I lied, one more random comment",
+    //   "I lied again, one more random comment",
+    //   "Why am I doing this?",
+    //   "I'm bored",
+    // ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[3],
+    name: "Jimmy Boss",
+    username: "jimmyBoss",
+    description:
+      `Poor #ETHBTC chart.
+
+      However, we are right at range low with a good support at 0.048. There is no point to short this ratio here.
+      
+      
+      I think we could at least have a small bounce to 0.05+, and then we can see if ETH bulls can actually make it or not.`,
+    picturePath: "4.jpg",
+    isBullish : '',
+    // userPicturePath: "p5.jpeg",
+    // likes: new Map([
+    //   [userIds[1], true],
+    //   [userIds[6], true],
+    //   [userIds[3], true],
+    //   [userIds[5], true],
+    // ]),
+    // comments: [
+    //   "one more random comment",
+    //   "I lied, one more random comment",
+    //   "I lied again, one more random comment",
+    //   "Why am I doing this?",
+    //   "I'm bored",
+    // ],
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    userId: userIds[4],
+    name: "Degen Ape Trader",
+    username: "Degenapetrader",
+    description:
+      `This is usually good local top signal for Gold around 2.3k.
+
+      Probably #BTC will keep going up from here while Gold just trades sideway.
+      
+      Seriously thinking about shorting Gold / Longing BTC trade combo 🤣.`,
+    picturePath: "5.png",
+    isBullish : '',
     // userPicturePath: "p5.jpeg",
     // likes: new Map([
     //   [userIds[1], true],
